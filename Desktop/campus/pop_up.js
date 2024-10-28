04ba5e66c8927dc6f25518a131c5c78cdccb4596
doggang <rlaehgusqp@naver.com>
2024-10-28 22:12:06 +0900

    파일 위치 변경

--- Desktop/campus/pop_up.js
+++
@@ -1,4 +0,0 @@
-            var a = document.getElementById(this);
-            function popup(){
-                window.open('test.html', '_blank', 'width=800, height=600'); return false;
-            }
