4ea53d9dc91e0928579e80694d5a9b3b563c31c1
doggang <rlaehgusqp@naver.com>
2024-11-02 23:36:59 +0900

    클릭시 점프까지 구현

--- script.js
+++ script.js
@@ -1,30 +1,8 @@
-var jumper = document.querySelector("#jumper");
+var jumper = document.getElementById("jumper");
 
-function jump(e){
-    const startPosition = parseInt(getComputedStyle(jumper).left);
-    const targetPosition = startPosition + 100; // 목표 위치
-    const duration = 1000; // 전환 시간 (1초)
-    const interval = 10; // 이동 간격 (ms)
-
-    let currentTime = 0;
-
-    if(e.keyCode == '32'){
-        console.dir(jumper.offsetTop);
-        currentTime += interval  
-
-        const progress = Math.min(currentTime / duration, 1); // 진행 상태 (0~1)
-
-        const currentPosition = startPosition + (targetPosition - startPosition) * progress;
-        jumper.style.left = currentPosition + 'px';
-
-        if (progress < 1) {
-          setTimeout(jump, interval);
-        }
-
-    }else{
-        console.log("error");
-    }
-};
-
-window.addEventListener("keydown", (e)=>{jump(e)});
-
+window.addEventListener("click", function(e){
+    jumper.style.transform = 'translate(0px, -300px)';
+    setTimeout(function(){
+        jumper.style.transform = 'translate(' +'0px', + '350px)';
+    },400);
+});
